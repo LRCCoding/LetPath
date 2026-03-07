b65a74888c08ced24f7194902a5b6e6e781a8565
Rudith45 <rudygatzke@hotmail.com>
2026-03-07 11:19:47 -0600

    robot header file

--- include/Robot.h
+++ include/Robot.h
@@ -1,11 +1,14 @@
 #pragma once
 
+namespace LetPath{
+
 class Robot{
     public:
         //Constructors
-        Robot();
         Robot(double WHEEL_SIZE, double TRACK_WIDTH);
+        Robot();
     private:
         double WHEEL_SIZE = 0;
         double TRACK_WIDTH = 0;
 };
+}
--- src/Robot.cpp
+++ src/Robot.cpp
@@ -2,8 +2,9 @@
 
 namespace LetPath{
 
-Robot::Robot() : WHEEL_SIZE(4) TRACK_WIDTH(15) {}
+Robot::Robot() : WHEEL_SIZE(0), TRACK_WIDTH(0) {}
 Robot::Robot(double wheelSize, double trackWidth) : WHEEL_SIZE(wheelSize), TRACK_WIDTH(trackWidth) {}
+
 
 
 }
