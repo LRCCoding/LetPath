38fa9c0e074c60b709b290984325f6ab92b3403d
Rudith45 <rudygatzke@hotmail.com>
2026-03-07 14:48:36 -0600

    Added Robot class

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,10 +1,20 @@
 #include "LetPath/Robot.h"
+#include "LetPath/Pose.h"
+#include "LetPath/Point.h"
 
 namespace LetPath{
 
 Robot::Robot() : WHEEL_SIZE(0), TRACK_WIDTH(0) {}
 Robot::Robot(double wheelSize, double trackWidth) : WHEEL_SIZE(wheelSize), TRACK_WIDTH(trackWidth) {}
 
+void moveToPoint(double x, double y, double theta){
+    Point target(x, y);
+    
+}
 
+void moveToPosition(double x, double y, double theta){
+    Pose target(x, y, theta);
+}
 
+void moveToPosition(){
 }
